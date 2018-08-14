4388431eecafb5f10b9567d18a0089bfc316e239
Danilo Dias <danilolive30@hotmail.com>
2018-08-14 19:48:06 -0300

    Update Nton.java

--- 20162/src/singletonInstancias/Nton.java
+++ 20162/src/singletonInstancias/Nton.java
@@ -15,33 +15,39 @@ import java.util.List;
  * @author LEESE
  */
 public class Nton {
-    List<Nton> lista;
-    private int min;
-    private int max;
+    private  static  List<Nton> lista = new ArrayList<>();
+    private  static int min;
+    private  static int max;
     private static Nton instance ;
-    private int count=0;
+    private  static int count=0;
     
-    private Nton(){
-        this.lista = new ArrayList<>();
+    private Nton(){}
+    
+    public static void initialize(int min, int max){
+        Nton.min = min; 
+        Nton.max = max;
+        for(int i=0;i<min;i++){
+            Nton.lista.add(new Nton());// cria as 3 intancias e deixa a lista com 3 posições
+        }
     }
     
-    public void initialize(int min, int max){
-        this.min = min; 
-        this.max = max;
-        for(int i=0;i<this.min;i++){
-            lista.add(new Nton());
-        }
+    public static int getSize(){
+        return Nton.lista.size();
     }
-   public static Nton getInstance(){
-        if(lista.size() < this.max){
-            lista.add(new Nton());
-            return lista.get(lista.size());
-            //return new Nton();
+    
+    public static Nton getInstance(){
+       if(Nton.count < Nton.min){
+           return Nton.lista.get(Nton.count++);
         }
-        if(this.count  == this.max){
-            this.count = 0;
+        if(Nton.count < Nton.max){
+            Nton.lista.add(new Nton());
+            Nton.count = Nton.getSize(); 
+            return Nton.lista.get(Nton.count-1);
         }
-        return lista.get(count++);
+        if((Nton.count) == Nton.max){
+             Nton.count = 0;
+        }
+        return Nton.lista.get(count++);
     }
     
 
