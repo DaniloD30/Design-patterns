21b57ee60278e68ffbc383dcacff1aaa060979b1
Danilo Dias <danilolive30@hotmail.com>
2018-08-16 23:11:28 -0300

    Update Run.java

--- 20162/Questão 1/src/testes/Run.java
+++ 20162/Questão 1/src/testes/Run.java
@@ -15,26 +15,19 @@ import singletonInstancias.Nton;
  */
 public class Run {
       public static void main(String[] args){
-            Nton.initialize(3, 5); 
-            // No mínimo três e no máximo cinco instâncias
-            // Três news já devem ocorrer nesta linha
-            Nton i1 = Nton.getInstance(); 
-            // Obtém a 1a instância (sem new)
-            Nton i2 = Nton.getInstance(); 
-        // Obtém a 2a instância (sem new)
-            Nton i3 = Nton.getInstance(); 
-        // Obtém a 3a instância (sem new)
-            Nton i4 = Nton.getInstance(); 
-        // Obtém a 4a instância (com new)
-            Nton i5 = Nton.getInstance(); 
-        // Obtém a 5a instância (com new)
-            Nton i6 = Nton.getInstance();
-            // Obtém a 1a instância
-            //if (i6 == i1) System.out.println(“OK”);
+        Nton.initialize(3, 5); // No mínimo três e no máximo cinco instância. Três news já devem ocorrer nesta linha
+        Nton i1 = Nton.getInstance();  // Obtém a 1a instância (sem new)
+        Nton i2 = Nton.getInstance();  // Obtém a 2a instância (sem new)
+        Nton i3 = Nton.getInstance(); // Obtém a 3a instância (sem new)
+        Nton i4 = Nton.getInstance();    // /Obtém a 4a instância (com new)
+        Nton i5 = Nton.getInstance();  // Obtém a 5a instância (com new)
+        Nton i6 = Nton.getInstance();   // Obtém a 1a instância
+           if (i6 == i1) 
+               System.out.println("Ok");
             // Deve exibir “OK”
-            Nton i7 = Nton.getInstance(); 
-        // Obtém a 2a instância
-            //if (i7 == i2) System.out.println(“OK”); 
+        Nton i7 = Nton.getInstance();  // Obtém a 2a instância
+            if (i7 == i2) 
+                System.out.println("OK"); 
            
       }
 }
